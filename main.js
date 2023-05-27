c7fdb4136a95a0afe900959f276f31fb5a1d8717
Marishef <marishef@bk.ru>
2023-05-27 16:01:24 +0300

    task2

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-let money = +prompt('Ваш бюджет на месяц?', '');
-let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money;
+let time;
+
+const start = function() {
+money = +prompt('Ваш бюджет на месяц?', '');
+time = prompt('Введите дату в формате YYYY-MM-DD', '');
+return money, time;
+
+
+
+}
 
 let appData = {
     budget: money,
@@ -9,65 +18,28 @@ let appData = {
     income: [],
     savings: false,
 }
-for (let i = 0; i < 2; i++) {
-    let quest1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-        quest2 = prompt('Во сколько обойдется?', '');
-    
-    if ( (typeof(quest1)) === 'string' && (typeof(quest1)) != null && 
-        (typeof(quest2)) != null && quest1 != '' && quest2 != '' && 
-        quest1.length < 50) {
-            appData.expenses[quest1] = quest2;
-        } else {
-            console.log ("bad result");
-            i--;
-        }
-};
 
-// let i = 0;
-// while (i < 2) {
-//     let quest1 = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-//         quest2 = prompt ("Во сколько обойдется?", "");
 
-//     if ( typeof(quest1)==='string' && typeof(quest1) != null && 
-//          typeof(quest2) != null && quest1 != "" && quest2 != "" 
-//          && quest1.length < 50) {
+const detectDayBudget = function () {
+    for (let i = 0; i < 2; i++) {
+        let quest1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+            quest2 = prompt('Во сколько обойдется?', '');
+        
+        if ( (typeof(quest1)) === 'string' && (typeof(quest1)) != null && 
+            (typeof(quest2)) != null && quest1 != '' && quest2 != '' && 
+            quest1.length < 50) {
+                appData.expenses[quest1] = quest2;
+            } else {
+                console.log ("bad result");
+                i--;
+            }
+    };
+    appData.moneyPerDay = appData.budget/30
+    alert ('Ежедневный бюджет: ' + appData.moneyPerDay);
+}
 
-//         console.log ("done");
 
-//         appData.expenses[quest1] = quest2;
-//     } else {
-//          console.log ("bad result");
-//          i--;
-//     }
-
-//     i++;
-// }
-
-// let i = 0;
-// do {
-//     let quest1 = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-//         quest2 = prompt ("Во сколько обойдется?", "");
-
-//     if ( typeof(quest1)==='string' && typeof(quest1) != null && 
-//          typeof(quest2) != null && quest1 != "" && 
-//          quest2 != "" && quest1.length < 50) {
-
-//         console.log ("done");
-
-//         appData.expenses[quest1] = quest2;
-//     } else {
-//          console.log ("bad result");
-//          i--;
-//     }
-
-//     i++;
-// }
-// while(i < 2);
-
-appData.moneyPerDay = appData.budget/30;
-
-alert('Ежедневный бюджет: ' + appData.moneyPerDay);
-
+const detectLevel = function () {
 if (appData.moneyPerDay < 100) {
     console.log('Минимальный уровень достатка')
 } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
@@ -77,3 +49,15 @@ if (appData.moneyPerDay < 100) {
 } else {
     console.log('Произошла ошибка');
 }
+};
+
+
+const chooseOptExpenses  = function () {
+    for (let i = 1; i <= 3; i++) {
+        let quest3 = prompt('Статья необязательных расходов?');
+    
+    appData.optinalExpenses[i] =  quest3;
+    }
+    console.log(appData.optinalExpenses);
+}
+chooseOptExpenses();
